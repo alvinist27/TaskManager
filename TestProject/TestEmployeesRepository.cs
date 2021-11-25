1a12f51d82b3a2fb8c4b99dfd57d3ec3e3e01bf9
Хотамов Сухроб <suhrobhotamov496@gmail.com>
2021-11-25 15:44:26 +0300

    TestEmployeesRepository

--- TestProject/TestEmployeesRepository.cs
+++ TestProject/TestEmployeesRepository.cs
@@ -13,8 +13,6 @@ namespace TestProject
     {
 
         Guid guid_value1 = new Guid("0DD62430-80CA-44A5-B16D-5FBF0432406B");
-        Guid guid_value2 = new Guid("6A96F425-C325-4B35-89CF-C3AC2AFD8B15");
-        Guid guid_value3 = new Guid("9DE3A2D8-94E6-440E-A57C-16F9AD9285B7");
 
         List<TaskManager.Domain.Task> list = new List<TaskManager.Domain.Task>();
 
@@ -32,19 +30,22 @@ namespace TestProject
             Assert.Equal("Nic", employeesRepository.GetByIdAsync(guid_value1).Result.EName);
         }
 
-        /*[Theory]
-        [InlineData(guid_value1, guid_value1)]
-        [InlineData(2, 2)]
-        [InlineData(3, 3)]
-        public void TestMultipleAdd(Guid id, Guid expected)
+        [Theory]
+        [InlineData("0DD62430-80CA-44A5-B16D-5FBF04324061", "0DD62430-80CA-44A5-B16D-5FBF04324061")]
+        [InlineData("0DD62430-80CA-44A5-B16D-5FBF04324062", "0DD62430-80CA-44A5-B16D-5FBF04324062")]
+        [InlineData("0DD62430-80CA-44A5-B16D-5FBF04324063", "0DD62430-80CA-44A5-B16D-5FBF04324063")]
+        public void TestMultipleAdd(string id, string expected)
         {
             var testHelper = new TestHelper();
             var employeesRepository = testHelper.EmployeesRepository;
 
-            Employee person = new Employee { EmployeeID = guid_value1, EName = "Nic", EWork = "Строитель", Tasks = list };
+            var id_1 = new Guid(id);
+            var expected_id = new Guid(expected);
+
+            Employee person = new Employee { EmployeeID = id_1, EName = "Nic", EWork = "Строитель", Tasks = list };
             employeesRepository.AddAsync(person).Wait();
 
-            Assert.Equal(expected, employeesRepository.GetByIdAsync(guid_value1).Result.EmployeeID);
-        }*/
+            Assert.Equal(expected_id, employeesRepository.GetByIdAsync(id_1).Result.EmployeeID);
+        }
     }
 }
