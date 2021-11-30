8fb5fb123b27945dba48d81c5a0dc2f000419699
Хотамов Сухроб <suhrobhotamov496@gmail.com>
2021-12-01 02:48:25 +0300

    Добавление тестов

--- TestProject/TestMissionRepository.cs
+++ TestProject/TestMissionRepository.cs
@@ -21,20 +21,12 @@ namespace TestProject
 
             Mission mission = new Mission
             {
-                MissionID = guid_value1,
-                MName = "Создать БД",
-                MDescription = "Создания БД с использованием SQLServer",
-                DateStart = new DateTime(2021, 11, 20),
-                DateEnd = new DateTime(),
-                Employee = new Employee(),
-                Project = new Project(),
-                EmployeeID = new Guid("0DD62430-80CA-44A5-B16D-5FBF04324223"),
-                ProjectID = new Guid("0DD62430-80CA-44A5-B16D-5FBF04324224"),
-                MStatus = "В процессе",
-                MType = "Технологический отдел"
+                MissionID = guid_value1, MName = "Создать БД", MDescription = "Создания БД с использованием SQLServer",
+                DateStart = new DateTime(2021, 11, 20), DateEnd = new DateTime(), Employee = new Employee(), Project = new Project(),
+                EmployeeID = new Guid("0DD62430-80CA-44A5-B16D-5FBF04324223"), ProjectID = new Guid("0DD62430-80CA-44A5-B16D-5FBF04324224"),
+                MStatus = "В процессе", MType = "Технологический отдел"
             };
-
-
+            
             missionsRepository.AddAsync(mission).Wait();
 
             Assert.Equal(guid_value1, missionsRepository.GetByIdAsync(guid_value1).Result.MissionID);
